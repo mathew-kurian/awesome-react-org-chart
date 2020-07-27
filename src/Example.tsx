77d30848d6bf8108908814098e3cf2cf396cdcfb
Mathew Kurian <mwkurian@gmail.com>
2020-07-27 14:57:47 -0500

    fix(animation): added collapse animation to example [skip release]

--- src/Example.tsx
+++ src/Example.tsx
@@ -11,6 +11,7 @@ import Grid from "react-fast-grid";
 import generateNodes, { Node, isNode } from "./generate-nodes";
 import Header from "./Header";
 import AnimatedNodeContainer from "./AnimatedNodeContainer";
+import { FadeTransform } from "react-animation-components";
 
 interface ExampleState {
   layout: LayoutType;
@@ -20,34 +21,75 @@ interface ExampleState {
 
 const CollapsedCards = () => (
   <>
-    <Card
+    <FadeTransform
       style={{
-        top: "-4%",
-        height: "92%",
-        left: "-4%",
-        width: "108%",
-        borderRadius: 8,
-        border: "none",
+        top: 0,
+        height: "100%",
+        left: "-0.5%",
+        width: "101%",
         position: "absolute",
-        boxShadow: "0 3px 3px rgba(0,0,0,0.2)",
-        background: "#fe3efa",
-        color: "rgba(255,255,255,0.75)",
       }}
-    />
-    <Card
+      enterTransform="translateY(9%)"
+      in
+    >
+      <Card
+        style={{
+          height: "100%",
+          width: "100%",
+          borderRadius: 8,
+          border: "none",
+          boxShadow: "0 3px 3px rgba(0,0,0,0.2)",
+          background: "#6a3fff",
+          color: "rgba(255,255,255,0.75)",
+        }}
+      />
+    </FadeTransform>
+    <FadeTransform
       style={{
-        top: "-2%",
-        height: "96%",
-        left: "-2%",
-        width: "104%",
-        borderRadius: 8,
-        border: "none",
+        top: 0,
+        height: "100%",
+        left: "-0.5%",
+        width: "101%",
         position: "absolute",
-        boxShadow: "0 3px 3px rgba(0,0,0,0.2)",
-        background: "#fea83e",
-        color: "rgba(255,255,255,0.75)",
       }}
-    />
+      enterTransform="translateY(6%)"
+      in
+    >
+      <Card
+        style={{
+          height: "100%",
+          width: "100%",
+          borderRadius: 8,
+          border: "none",
+          boxShadow: "0 3px 3px rgba(0,0,0,0.2)",
+          background: "#fe3efa",
+          color: "rgba(255,255,255,0.75)",
+        }}
+      />
+    </FadeTransform>
+    <FadeTransform
+      style={{
+        top: 0,
+        height: "100%",
+        left: "-0.5%",
+        width: "101%",
+        position: "absolute",
+      }}
+      enterTransform="translateY(3%)"
+      in
+    >
+      <Card
+        style={{
+          height: "100%",
+          width: "100%",
+          borderRadius: 8,
+          border: "none",
+          boxShadow: "0 3px 3px rgba(0,0,0,0.2)",
+          background: "#fea83e",
+          color: "rgba(255,255,255,0.75)",
+        }}
+      />
+    </FadeTransform>
   </>
 );
 
